9811340ef07f210e92c70a658b30286743a0fa86
Lachlan Young <lachlanbyoung@gmail.com>
2024-05-07 14:59:12 +1000

    Revert "✨ Try google analytics consent"
    This reverts commit 4d3814e0cbe0e247c8686ea0f0b7aeaf173b7b9b.

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,11 @@
-import React, { useEffect } from 'react';
-import CookieConsent, {
-  getCookieConsentValue,
-  Cookies,
-} from "react-cookie-consent";
-import * as ReactGA from "react-ga";
-
+import React from 'react';
 import Layout from './components/Layout';
 import DiffForm from './components/DiffForm';
 
-const handleAcceptCookie = () => {
-  ReactGA.initialize('G-CWNYFNSFQH');
-};
-
-const handleDeclineCookie = () => {
-  //remove google analytics cookies
-  Cookies.remove("_ga");
-  Cookies.remove("_gat");
-  Cookies.remove("_gid");
-};
-
 const App = () => {
-  useEffect(() => {
-    const isConsent = getCookieConsentValue();
-    if (isConsent === "true") {
-      handleAcceptCookie();
-    }
-  }, []);
-
   return (
     <Layout title="RubyHash" subtitle="Welcome to RubyHash">
       <DiffForm />
-      <CookieConsent
-        enableDeclineButton
-        onAccept={handleAcceptCookie}
-        onDecline={handleDeclineCookie}
-      >
-        This website uses cookies to enhance the user experience.
-      </CookieConsent>
     </Layout>
   );
 };
