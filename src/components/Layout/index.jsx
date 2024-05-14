2c40a1486a31ced52a3932ec2e886251bb7a05ba
Lachlan Young <lachlanbyoung@gmail.com>
2024-05-14 10:57:13 +1000

    ✨ some words

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -6,7 +6,22 @@ const Layout = ({ title, subtitle, children }) => {
     <div className={styles.container}>
       <header>
         <h1>{title}</h1>
-        <p>{subtitle}</p>
+        <p>
+          In testing frameworks like Minitest, when a test involving hashes fails, 
+          Minitest helps developers by displaying a diff of the expected and actual 
+          hashes. This diff highlights the differences between the two hashes, 
+          making it easier to identify discrepancies.
+        </p>
+        <p>
+          In testing frameworks like Minitest, when a test involving hashes fails, 
+          Minitest helps developers by displaying a diff of the expected and actual hashes. 
+          This diff <strong>does not</strong> highlight the differences between the two hashes, 
+          making it difficult to identify discrepancies. Rubyhash is here to help show the difference
+        </p>
+        <p>
+          To use RubyHash, please paste the hash contents directly from console taking care to include the
+          '-' and '+' markers for us to compare the two hashes correctly. 
+        </p>
       </header>
       <main>
         <div className={styles.content}>{children}</div>
